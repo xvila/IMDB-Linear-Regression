5a487b4c57649cb1b2005e7bb8f0b9eded385676
Xavier Vila <xavier_vila@fastmail.com>
2017-10-07 12:12:02 -0400

    readme and cleanup

---
+++ README.md
@@ -0,0 +1,11 @@
+# Predicting Box Office Gross with IMDB
+
+As my first Linear Regression project I attempted to predict the domestic box
+office gross of movies scraped from [IMDB](http://www.imdb.com). I specifically
+focused on a list that contained movies that were based on books. That list can
+be found
+[here](http://www.imdb.com/search/keyword?keywords=based-on-novel&mode=detail&page=1&ref_=kw_nxt&sort=moviemeter,asc).
+
+## Tools Used
+
+* [Scrapy](https://scrapy.org) - Web scraper used for IMDB
---
+++ movies/movies/spiders/movie_novel_scrape_with_scores.py
@@ -0,0 +1,107 @@
+import scrapy
+
+class MovieSpider(scrapy.Spider):
+    name = 'movie_novel_score_scrape'
+
+    custom_settings = {
+        # "DOWNLOAD_DELAY": 3,
+        "CONCURRENT_REQUESTS_PER_DOMAIN": 3,
+        "HTTPCACHE_ENABLED": True
+    } 
+    start_urls = []
+    
+    for i in range(1,612):
+        #base = 'http://www.imdb.com/search/title?release_date={}'
+        base = 'http://www.imdb.com/search/keyword?keywords=based-on-novel&mode=detail&page={}&ref_=kw_nxt&sort=moviemeter,asc'
+        url = base.format(i)
+        start_urls.append(url)
+
+    def parse(self, response):
+        # Extract the links to the individual movie
+        static_movie_url = 'http://www.imdb.com'
+        static_list_url = 'http://www.imdb.com/search/title'
+
+        for href in response.xpath('//h3/a/@href').extract():
+            href = static_movie_url + href
+            yield scrapy.Request(
+                url=href, callback=self.parse_movie,
+                meta={'url':href})
+        
+        # Follow pagination links and repeat
+        # next_url = response.xpath(
+        #     '//div[@class="desc"]/a/@href'
+        # )[1].extract()
+        # next_url = static_list_url + next_url
+        # yield scrapy.Request(url=next_url,callback=self.parse)
+
+    def parse_movie(self,response): 
+        url = response.request.meta['url']
+        try:
+            name = response.xpath('//div[@class="title_wrapper"]/h1/text()').extract()[0].strip()
+        except:
+            name = None
+        try:
+            budget = response.xpath('//div[@class="txt-block"]/h4/text()[contains(.,"Budget")]/../../text()').extract()[1].strip()
+        except:
+            budget = None
+        try:
+            box_office = response.xpath('//div[@class="txt-block"]/h4/text()[contains(.,"Gross")]/../../text()').extract()[1].strip()
+        except:
+            box_office = None
+        try:
+            release_date = response.xpath('//div[@class="txt-block"]/h4/text()[contains(.,"Release Date:")]/../../text()').extract()[1].strip()
+        except:
+            release_date = None
+        try:
+            user_rating = response.xpath('//div[@class="ratingValue"]//span/text()').extract()[0]
+        except:
+            user_rating = None
+        try:
+            genre = response.xpath('//div[@itemprop="genre"]/a/text()').extract()
+        except:
+            genre = None
+        try:
+            year = response.xpath('//span[@id="titleYear"]/a/text()').extract()[0]
+        except:
+            year = None
+        try:
+            rating = response.xpath('//meta[@itemprop="contentRating"]/../text()').extract()[1].strip()
+        except:
+            rating = None
+        try:
+            runtime = response.xpath('//div[@class="txt-block"]/time/text()').extract()[0]
+        except:
+            runtime = None
+        try:
+            director = response.xpath('//span[@itemprop="director"]/a/span/text()').extract()[0]
+        except:
+            director = None
+        try:
+            meta_rating = response.xpath('//div[@class="metacriticScore score_favorable titleReviewBarSubItem"]/span/text()').extract()[0]
+        except:
+            meta_rating = None
+        try:
+            user_rating_count = response.xpath('//span[@itemprop="ratingCount"]/text()').extract()[0]
+        except:
+            user_rating_count = None
+        try:
+            opening = response.xpath('//div[@class="txt-block"]/h4/text()[contains(.,"Opening Weekend")]/../../text()').extract()[1].strip()
+        except:
+            opening = None
+     
+        yield{
+        'url': url,
+        'name': name,
+        'budget': budget,
+        'box_office': box_office,
+        'opening': opening,
+        'user_rating': user_rating,
+        'genre': genre,
+        'year': year,
+        'rating': rating,
+        'runtime': runtime,
+        'director': director,
+        'release_date': release_date,
+        'meta_rating':meta_rating,
+        'user_rating_count':user_rating_count
+        }
